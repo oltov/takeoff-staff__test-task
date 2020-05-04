d98fb5115b7fa97dc31158f2fe9ffca7ee92ea76
oltov <ururairurai@gmail.com>
2020-05-05 01:21:29 +0300

    переделал шапку и поиск на ui vuetify

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,26 +1,104 @@
 <template>
-  <div class="home">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand">Меню контактов</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <v-card>
+    <v-app-bar max-width="1100" app class="mx-auto cyan lighten-1" dark>
+      <v-toolbar-title>
+        Контакты
+      </v-toolbar-title>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline my-2 my-lg-0" @submit.prevent="addingContact">
-          <div class="row">
-            <div class="col">
+      <v-spacer />
+
+      <!-- ===== кнопка выхода из приложения ===== -->
+      <v-btn class="caption" @click="logOut" color="cyan darken-2">
+        <v-icon dark left>mdi-arrow-left</v-icon>выход
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-card
+      max-width="1100"
+      class="d-flex justify-space-around pa-12 mt-6 mx-auto"
+      color="cyan lighten-4"
+    >
+
+      <!-- ===== форма добавления контактов ===== -->
+
+      <ValidationObserver v-slot="{ handleSubmit }" slim>
+        <v-form @submit.prevent="handleSubmit(addingContact)">
+          <v-row class="mt-5">
+            <ValidationProvider
+              rules="alpha"
+              v-slot="{ errors }"
+              name="Имя"
+              mode="eager"
+            >
+              <v-text-field
+                v-model="contactUser.name"
+                :error-messages="errors"
+                class="mr-5"
+                placeholder="Имя"
+              />
+            </ValidationProvider>
+
+            <ValidationProvider
+                rules="alpha"
+                v-slot="{ errors }"
+                name="Телефон"
+                mode="eager"
+              >
+              <v-text-field
+                v-model="contactUser.phoneNumber"
+                :error-messages="errors"                
+                placeholder="Телефон"
+              />
+            </ValidationProvider>
+            <v-card-actions>
+              <v-btn class="caption" type="submit" color="cyan darken-1" dark>добавить контакт</v-btn>
+            </v-card-actions>
+          </v-row>
+        </v-form>
+      </ValidationObserver>
+
+      <!-- ===== форма поиска контактов ===== -->
+      <ValidationObserver v-slot="{ handleSubmit }" slim>
+        <v-form @submit.prevent="handleSubmit(search)">
+          <v-row class="mt-5">
+            <ValidationProvider
+                rules="alpha"
+                v-slot="{ errors }"
+                name="Пароль"
+                mode="eager"
+                vid="password"
+              >
+              <v-text-field
+                v-model="searchValue"
+                :error-messages="errors"
+                
+              />
+            </ValidationProvider>
+            <v-card-actions>
+              <v-btn class="caption" type="submit" color="cyan darken-1" dark>поиск</v-btn>
+            </v-card-actions>
+            <v-card-actions
+              v-if="isSearching"
+              @click="getAllContacts"
+            >
+              <v-btn class="caption" type="submit" color="cyan darken-1" dark>показать все контакты</v-btn>
+            </v-card-actions>
+          </v-row>
+        </v-form>
+      </ValidationObserver>
+
+    </v-card>
+
+    <!-- <nav class="">
+
+      <div class="" id="navbarSupportedContent">
+        <form class="" @submit.prevent="addingContact">
+          <div class="">
+            <div class="">
               <input v-model="contactUser.name" type="text" class="form-control" placeholder="Имя" />
             </div>
-            <div class="col">
+            <div class="">
               <input
                 v-model="contactUser.phoneNumber"
                 type="text"
@@ -28,84 +106,78 @@
                 placeholder="Телефонный номер"
               />
             </div>
-            <div class="col">
+            <div class="">
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Добавить контакт</button>
             </div>
           </div>
         </form>
       </div>
-      <div>
-        <form @submit.prevent="search" class="form-inline my-2 my-lg-0">
-          <div class="col">
-            <input
-              v-model="searchValue"
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Поиск"
-              aria-label="Поиск"
-            />
-          </div>
-          <div class="col">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск контактов</button>
-          </div>
-        </form>
-      </div>
-      <div class="ml-2">
-        <button @click="logOut" type="button" class="btn btn-primary">Выход</button>
-      </div>
-    </nav>
-    <button
-      v-if="isSearching"
-      @click="getAllContacts"
-      type="button"
-      class="btn btn-primary btn-lg btn-block"
-    >Загрузить все контакты</button>
-    <div class="d-flex flex-row flex-wrap bd-highlight mb-3 pt-4">
+    </nav> -->
+    <v-card
+      max-width="1100"
+      class="pa-12 mx-auto"
+      color="yellow lighten-5"
+    >
       <div
         v-for="(contact, index) in getContacts"
         :key="index"
-        class="card mr-2 mb-3 p-2"
+        class=""
         style="width: 14rem;"
       >
-        <img :src="contact.image" class="card-img-top" :alt="contact.name" />
-        <div class="card-body p-0">
+        <img :src="contact.image" class="" :alt="contact.name" />
+        <div class="">
           <div>
-            <h5 v-if="!contact.isEdit" class="card-title">{{ contact.name }}</h5>
+            <h5 v-if="!contact.isEdit" class="">{{ contact.name }}</h5>
             <input v-else type="text" v-model="contact.name" />
           </div>
           <div>
-            <p v-if="!contact.isEdit" class="card-text">{{ contact.phoneNumber }}</p>
+            <p v-if="!contact.isEdit" class="">{{ contact.phoneNumber }}</p>
             <input v-else type="text" v-model="contact.phoneNumber" />
           </div>
-          <div class="btn-group w-100" role="group" aria-label="Basic example">
+          <div class="" role="group" aria-label="Basic example">
             <div>
               <button
                 @click="editingContact(contact)"
                 type="button"
-                class="btn btn-success btn-sm mr-1"
+                class=""
                 :class="{ 'btn-warning': contact.isEdit }"
               >{{ !contact.isEdit ? 'Редактировать' : 'Сохранить'}}</button>
             </div>
             <button
               @click="deleteData(contact.id)"
               type="button"
-              class="btn btn-success btn-sm"
+              class=""
               :disabled="contact.isEdit"
             >Удалить</button>
           </div>
         </div>
       </div>
-    </div>
-  </div>
+    </v-card>
+  </v-card>
 </template>
 
 <script>
+import {
+  extend,
+  localize,
+  ValidationProvider,
+  ValidationObserver
+} from "vee-validate";
+import ru from "vee-validate/dist/locale/ru.json";
+
+import { required, alpha } from "vee-validate/dist/rules";
+
 import faker from "faker";
 import { mapGetters, mapMutations, mapActions } from "vuex";
 faker.locale = "ru";
 
 export default {
   name: "Contacts",
+    components: {
+    ValidationProvider,
+    ValidationObserver
+  },
+
   data: () => ({
     contactUser: {
       id: faker.random.uuid(),
