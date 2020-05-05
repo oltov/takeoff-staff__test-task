218efffd0ae20955a3246dcf754213bc0a0d8397
oltov <ururairurai@gmail.com>
2020-05-05 11:51:37 +0300

    передалал UI на vuetify

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -7,6 +7,39 @@
 
       <v-spacer />
 
+      <!-- ===== форма поиска контактов ===== -->
+
+      <ValidationObserver v-slot="{ handleSubmit }" slim>
+        <v-form @submit.prevent="handleSubmit(search)">
+          <v-row class="mt-0">
+            <ValidationProvider
+                rules="alpha"
+                v-slot="{ errors }"
+                name="Пароль"
+                mode="eager"
+                vid="password"
+              >
+              <v-text-field
+                v-model="searchValue"
+                :error-messages="errors"
+                class="mt-6"
+              />
+            </ValidationProvider>
+            <v-card-actions>
+              <v-btn class="caption" type="submit" color="cyan darken-1" dark>поиск</v-btn>
+            </v-card-actions>
+            <v-card-actions
+              v-if="isSearching"
+              @click="getAllContacts"
+            >
+              <v-btn class="caption" type="submit" color="cyan darken-1" dark>показать все контакты</v-btn>
+            </v-card-actions>
+          </v-row>
+        </v-form>
+      </ValidationObserver>
+
+      <v-spacer />
+
       <!-- ===== кнопка выхода из приложения ===== -->
       <v-btn class="caption" @click="logOut" color="cyan darken-2">
         <v-icon dark left>mdi-arrow-left</v-icon>выход
@@ -58,101 +91,56 @@
         </v-form>
       </ValidationObserver>
 
-      <!-- ===== форма поиска контактов ===== -->
-      <ValidationObserver v-slot="{ handleSubmit }" slim>
-        <v-form @submit.prevent="handleSubmit(search)">
-          <v-row class="mt-5">
-            <ValidationProvider
-                rules="alpha"
-                v-slot="{ errors }"
-                name="Пароль"
-                mode="eager"
-                vid="password"
-              >
-              <v-text-field
-                v-model="searchValue"
-                :error-messages="errors"
-                
-              />
-            </ValidationProvider>
-            <v-card-actions>
-              <v-btn class="caption" type="submit" color="cyan darken-1" dark>поиск</v-btn>
-            </v-card-actions>
-            <v-card-actions
-              v-if="isSearching"
-              @click="getAllContacts"
-            >
-              <v-btn class="caption" type="submit" color="cyan darken-1" dark>показать все контакты</v-btn>
-            </v-card-actions>
-          </v-row>
-        </v-form>
-      </ValidationObserver>
 
     </v-card>
 
-    <!-- <nav class="">
-
-      <div class="" id="navbarSupportedContent">
-        <form class="" @submit.prevent="addingContact">
-          <div class="">
-            <div class="">
-              <input v-model="contactUser.name" type="text" class="form-control" placeholder="Имя" />
-            </div>
-            <div class="">
-              <input
-                v-model="contactUser.phoneNumber"
-                type="text"
-                class="form-control"
-                placeholder="Телефонный номер"
-              />
-            </div>
-            <div class="">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Добавить контакт</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </nav> -->
     <v-card
       max-width="1100"
-      class="pa-12 mx-auto"
+      class="d-flex flex-wrap mx-auto"
       color="yellow lighten-5"
     >
-      <div
+      <v-card
+        max-width="200"
         v-for="(contact, index) in getContacts"
         :key="index"
-        class=""
-        style="width: 14rem;"
+        class="pa-1 mr-4 mb-4"
       >
-        <img :src="contact.image" class="" :alt="contact.name" />
-        <div class="">
-          <div>
-            <h5 v-if="!contact.isEdit" class="">{{ contact.name }}</h5>
-            <input v-else type="text" v-model="contact.name" />
-          </div>
-          <div>
-            <p v-if="!contact.isEdit" class="">{{ contact.phoneNumber }}</p>
-            <input v-else type="text" v-model="contact.phoneNumber" />
-          </div>
-          <div class="" role="group" aria-label="Basic example">
-            <div>
-              <button
-                @click="editingContact(contact)"
-                type="button"
-                class=""
-                :class="{ 'btn-warning': contact.isEdit }"
-              >{{ !contact.isEdit ? 'Редактировать' : 'Сохранить'}}</button>
-            </div>
-            <button
-              @click="deleteData(contact.id)"
-              type="button"
-              class=""
-              :disabled="contact.isEdit"
-            >Удалить</button>
-          </div>
+        <v-img :src="contact.image" height="200px" class="white--text align-end" :alt="contact.name" />
+        <div>
+          <v-card-title v-if="!contact.isEdit" class="pa-0">{{ contact.name }}</v-card-title>
+          <input v-else type="text" v-model="contact.name" />
         </div>
-      </div>
+        <div>
+          <v-card-subtitle v-if="!contact.isEdit" class="pa-0">{{ contact.phoneNumber }}</v-card-subtitle>
+          <input v-else type="text" v-model="contact.phoneNumber" />
+        </div>
+        <v-card-actions class="px-0">
+          <v-btn
+            :class="{ 'cyan lighten-4': !contact.isEdit, 'warning': contact.isEdit }"
+            @click="editingContact(contact)"
+            class="caption pa-0 px-1"
+          >
+            {{ !contact.isEdit ? 'Редактировать' : 'Сохранить'}}
+          </v-btn>
+
+          <v-btn
+            color="cyan lighten-4"
+            @click="deleteData(contact.id)"
+            :disabled="contact.isEdit"
+            class="caption pa-0 px-1 ml-auto"
+          >
+            Удалить
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-card>
+    <v-alert
+      v-if="getContacts.length < 1"
+      type="info"
+      max-width="1100"
+      class="mx-auto"
+    >
+      Контакт не найден, попробуйте ввести другое имя</v-alert>
   </v-card>
 </template>
 
